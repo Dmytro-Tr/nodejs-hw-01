a6e07f5ebd3b847283d9cc2082c6d8b139dd3c3e
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-21 18:01:36 +0200

    step 8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.lengty === 0) {
+      console.log('Contacts list is empty!');
+      return;
+    }
+
+    contacts.pop();
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error('Remove last Contatc: ', error);
+  }
+};
 
 removeLastContact();
