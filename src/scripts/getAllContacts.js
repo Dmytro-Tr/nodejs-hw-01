0b16ffca8db045433ab71f30290b37d0bbe8657a
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-21 16:03:49 +0200

    Step 5

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    return contacts;
+  } catch (error) {
+    console.log('Get all Contacts: ', error);
+  }
+};
 
 console.log(await getAllContacts());
